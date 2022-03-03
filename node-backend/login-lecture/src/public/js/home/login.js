c21b1309cb943dda2f08438a9b4d3cb45c4ffca4
hothoony <hothoony@hanmail.net>
2022-03-03 11:43:41 +0900

    .

--- node-backend/login-lecture/src/public/js/home/login.js
+++ node-backend/login-lecture/src/public/js/home/login.js
@@ -6,6 +6,19 @@ const loginBtn = document.querySelector('button');
 loginBtn.addEventListener('click', (e) => {
     const userid = document.querySelector('input[name=userid]');
     const userpw = document.querySelector('input[name=userpw]');
-    console.log(userid.value);
-    console.log(userpw.value);
+    const req = {
+        userid: userid.value,
+        userpw: userpw.value
+    };
+    console.log(req);
+    console.log(JSON.stringify(req));
+
+    // send to server
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 });
