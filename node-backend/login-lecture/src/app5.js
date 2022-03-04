73b5276dd8d596d92cdf6b6668a89156ad8c1ac2
hothoony <hothoony@hanmail.net>
2022-03-04 11:31:40 +0900

    .

--- node-backend/login-lecture/src/app5.js
+++ node-backend/login-lecture/src/app5.js
@@ -4,16 +4,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// view path
-app.set('views', './src/views');
 // view template
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
-// static file path
+
+// static resource
 app.use(express.static(`${__dirname}/public`));
+
 // bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
 
 const home = require('./routes/index');
 app.use('/', home);
