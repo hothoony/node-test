e04c593a0b6720f3f9f89e08fa6263331033be30
hothoony <hothoony@hanmail.net>
2022-03-14 12:05:44 +0900

    .

---
+++ web/callback-promise-async-await/async-await.js
@@ -0,0 +1,39 @@
+console.log('start');
+
+function loginUser(id, pw) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ name: 'james' });
+        }, 3000);
+    });
+}
+
+function getTeams(userName) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([{ teamId: '11' }, { teamId: '22' }]);
+        }, 3000);
+    });
+}
+
+function getTeamName(teamId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ teamName: 'teamA' });
+        }, 2000);
+    });
+}
+
+// async, await
+async function doLoginUser() {
+    const user = await loginUser('aa', 'bb');
+    console.log('login ok', user);
+    const teams = await getTeams(user.userName);
+    console.log('team list ok', teams);
+    const team = await getTeamName(teams[0].teanId);
+    console.log('team info ok', team.teamName);
+}
+
+doLoginUser();
+
+console.log('end');
