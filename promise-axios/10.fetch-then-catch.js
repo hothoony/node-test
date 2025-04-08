fb02d12e5356164a45f8d80a61fd74abe4c49ba8
hothoony <hothoony@hanmail.net>
2025-04-08 13:24:16 +0900

    .

--- promise-axios/10.fetch-then-catch.js
+++ promise-axios/10.fetch-then-catch.js
@@ -1,35 +1,35 @@
 
 const getData = async () => {
 
-    // const statusList = [200, 200, 200];
+    const statusList = [200, 200, 200];
     // const statusList = [500, 200, 200];
-    const statusList = [200, 500, 200];
+    // const statusList = [200, 500, 200];
     // const statusList = [200, 200, 500];
 
     fetch(`https://dummyjson.com/http/${statusList[0]}`)
     .then(async res => {
 
         const data1 = await res.json();
-        console.log('data1=', data1);
         if (data1.status === 500) {
-            throw new Error(data1.message);
+            throw new Error('data1:' + data1.message);
         }
+        console.log('data1=', data1);
 
         fetch(`https://dummyjson.com/http/${statusList[1]}`)
         .then(async res => {
             const data2 = await res.json();
-            console.log('data2=', data2);
             if (data2.status === 500) {
-                throw new Error(data2.message);
+                throw new Error('data2:' + data2.message);
             }
+            console.log('data2=', data2);
 
             fetch(`https://dummyjson.com/http/${statusList[2]}`)
             .then(async res => {
                 const data3 = await res.json();
-                console.log('data3=', data3);
                 if (data3.status === 500) {
-                    throw new Error(data3.message);
+                    throw new Error('data3:' + data3.message);
                 }
+                console.log('data3=', data3);
             })
             .catch(error3 => {
                 console.error('error3=', error3);
