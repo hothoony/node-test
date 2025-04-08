934a250a98bc1f483679731fffbb5af5e66af1cd
hothoony <hothoony@hanmail.net>
2025-04-08 13:31:52 +0900

    .

---
+++ promise-axios/20.axios-then-catch.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+const getData = async () => {
+
+    const statusList = [200, 200, 200];
+    // const statusList = [500, 200, 200];
+    // const statusList = [200, 500, 200];
+    // const statusList = [200, 200, 500];
+
+    axios.get(`https://dummyjson.com/http/${statusList[0]}`)
+    .then(async data1 => {
+        console.log('data1=', data1);
+
+        axios.get(`https://dummyjson.com/http/${statusList[1]}`)
+        .then(async data2 => {
+            console.log('data2=', data2);
+
+            axios.get(`https://dummyjson.com/http/${statusList[2]}`)
+            .then(async data3 => {
+                console.log('data3=', data3);
+            })
+            .catch(error3 => {
+                console.error('error3=', error3);
+            });
+
+        })
+        .catch(error2 => {
+            console.error('error2=', error2);
+        });
+
+    })
+    .catch(error1 => {
+        console.error('error1=', error1);
+    });
+}
+
+getData();
