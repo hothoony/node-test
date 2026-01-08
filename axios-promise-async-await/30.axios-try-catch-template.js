c055b75c750f41df982294bab387e5766ff6f86b
hothoony <hothoony@hanmail.net>
2026-01-08 23:11:36 +0900

    .

--- axios-promise-async-await/30.axios-try-catch-template.js
+++ axios-promise-async-await/30.axios-try-catch-template.js
@@ -1,39 +1,50 @@
 const axios = require('axios');
 
-async function callApi(url) {
+async function callApi(url, param = {}) {
 
     // const url = '';
-    const param = {};
-    const config = {};
+    // const param = {};
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer token',
+        },
+        timeout: 5000,
+    };
 
     try {
         const res = await axios.post(url, param, config);
         console.log('res', res);
         const result = res.data;
         console.log('result', result);
-        if (result.resultCd == '0000' || result.status == '200') {
-            // 200 OK, 성공
+        if (result.resultCd === '0000' || result.status === 200) {
+            // 업무 성공
             console.log('업무 성공');
             return result;
         } else {
-            // 200 OK, 실패
+            // 업무 실패
             console.warn('업무 실패');
         }
     } catch (err) {
         if (err.response) {
+            // 서버 응답 있음
             console.error('err #1');
             console.error('err.response.status =>', err.response.status);
             console.error('err =>', err);
         } else if (err.request) {
+            // 서버 응답 없음
             console.error('err #2');
             console.error('err.request =>', err.request);
             console.error('err.message =>', err.message);
             console.error('err =>', err);
         } else {
+            // 요청 오류
             console.error('err #3');
             console.error('err.message =>', err.message);
             console.error('err =>', err);
         }
+        // 호출한 쪽에 오류 처리를 위임함
+        throw err;
     }
 }
 
